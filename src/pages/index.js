5e287115f36fb5c93ea819404d373bf907866ea4
opencogmind <autoagi.university@gmail.com>
2023-11-20 13:06:56 +0500

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className=" button button--secondary button--lg"
-            to="/MoonBase/intro" style={{color: 'black', border: '2px solid purple' }}>
+            to="/Events/welcome" style={{color: 'black', border: '2px solid purple' }}>
             Findout
           </Link>
         </div>
